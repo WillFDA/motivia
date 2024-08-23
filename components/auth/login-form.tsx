a3ddbf69efddec36a4f259977cfe2a16bfb7a3df
William <william.deazevedopro@gmail.com>
2024-08-23 10:55:32 +0200

    auth login form debut

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,5 +1,26 @@
+"use client";
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas/index";
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
