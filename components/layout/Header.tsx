0fbe6c0ac76badcf1f39355f5904ff64fc8b5311
William <william.deazevedopro@gmail.com>
2024-09-25 21:07:31 +0200

    mm

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,7 +2,7 @@ import ToggleTheme from "../custom/ToggleTheme";
 export default function Header() {
   return (
     <header className="w-full flex justify-between">
-      <h1>Hello</h1>
+      <h1 className="font-bold text-2xl">Motivia</h1>
       <ToggleTheme />
     </header>
   );
